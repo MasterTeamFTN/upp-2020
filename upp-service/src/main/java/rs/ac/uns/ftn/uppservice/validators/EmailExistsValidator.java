33686d2d0bad24ecfd4c6f997bd87fed74057087
Boris Sulicenko <b.sulicenko@gmail.com>
2020-12-16 15:20:17 +0100

    Created custom username and email validation for user registration. Also created class for IoC workaround in special cases like in this one.

---
+++ upp-service/src/main/java/rs/ac/uns/ftn/uppservice/validators/EmailExistsValidator.java
@@ -0,0 +1,22 @@
+package rs.ac.uns.ftn.uppservice.validators;
+
+import org.camunda.bpm.engine.impl.form.validator.FormFieldValidator;
+import org.camunda.bpm.engine.impl.form.validator.FormFieldValidatorContext;
+import rs.ac.uns.ftn.uppservice.config.SpringContext;
+import rs.ac.uns.ftn.uppservice.repository.UserRepository;
+
+public class EmailExistsValidator implements FormFieldValidator {
+
+    private UserRepository userRepository;
+
+    public EmailExistsValidator() {
+        this.userRepository = SpringContext.getBean(UserRepository.class);
+    }
+
+    // Returns true if form field is valid
+    @Override
+    public boolean validate(Object o, FormFieldValidatorContext formFieldValidatorContext) {
+        String email = (String) o;
+        return !userRepository.findByEmail(email).isPresent();
+    }
+}
